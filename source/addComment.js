e605ccbc6e8b04ca6afbf80793dc882272f6679c
nico24687 <theonlynico@hotmail.com>
2018-03-09 20:03:54 -0700

    completed challenge using ES6

--- source/addComment.js
+++ source/addComment.js
@@ -1,3 +1,23 @@
 $(document).ready(function(){
-  // add your code here
+  $('#new_comment_button').click(() => {
+    $("#new_comment_button").hide()
+    $("#comments").append("<form id='new_comment'></form>")
+    $("#new_comment").show()
+    $("#new_comment").append('<textarea name="comment" placeholder="Post content">')
+      .append('<input type="text" name="authorName" placeholder="Author name">')
+      .append('<input type="submit" id="submit_comment">')
+
+    $("#new_comment").submit( () => {
+      const comment = $("textarea[name=comment]").val()
+      const name = $("input[name=authorName]").val()
+      if(comment.length === 0){
+        alert("You must provide a comment")
+      } else {
+        $("#new_comment_button").show()
+        $("#comment_list").append("<li>" + comment + "<span class='author'>" + name + "</span></li>")
+        $("#new_comment").remove()
+        event.preventDefault();
+      }
+    })
+  })
 })
